d0f3cd5c9a891412a57820a3ce2809865c73b992
Ganitha <t.ganith@gmail.com>
2020-02-07 22:00:28 +0530

    implement spring boot application

---
+++ src/test/java/com/myexamples/employeeservices/EmployeeservicesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.myexamples.employeeservices;
+
+/*
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
